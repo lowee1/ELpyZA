187559d24aed76d544e315ef2b10ac485d5a5a36
lowee1 <63391242+lowee1@users.noreply.github.com>
2021-07-16 10:29:51 +0800

    added test script called echo

--- elpyza_scripts.py
+++ elpyza_scripts.py
@@ -1,28 +1,61 @@
-class elpyza_script():
-    def __init__(self, user_prompt, description):
+class elpyza_script:
+    def __init__(self, user_prompt, description, initial, script_prefix):
         self.active = True
         self.user_prompt = user_prompt
         self.description = description
+        self.initial = initial
+        self.script_prefix = script_prefix
 
-    def parse_input():
-        pass
-
-    def get_script_response():
-        pass
+    def get_script_response(self, user_input: str) -> str:
+        raise NotImplementedError(
+            "get_script_response has not been implemented by child class yet"
+        )
 
 
 class doctor(elpyza_script):
     def __init__(self):
-        description = """This is an attempt at reproducing DOCTOR. 
-The original was created by Joseph Weizenbaum and was supposed to simulate a Rogerian psychotherapist."""
-        super().__init__(user_prompt="DOCTOR>", description=description)
+        description = """\
+This is an attempt at reproducing DOCTOR.\
+ The original was created by Joseph Weizenbaum and was supposed to simulate a\
+ Rogerian psychotherapist.\
+"""
+        initial = "Is something troubling you?"
+        super().__init__(
+            user_prompt="YOU: ",
+            description=description,
+            initial=initial,
+            script_prefix="DOCTOR: ",
+        )
 
 
-scripts = {'doctor': doctor()}
+class echo(elpyza_script):
+    def __init__(self):
+        description = (
+            """This is a test script which just repeats the users last input"""
+        )
+        super().__init__(
+            user_prompt="YOU: ",
+            description=description,
+            initial="echo test script",
+            script_prefix="ECHO: ",
+        )
+        self.previous_input = ""
+
+    def get_script_response(self, user_input: str) -> str:
+        if user_input in ["quit", "exit", "stop", "go away"]:
+            self.active = False
+            return "ok bye"
+        response = self.previous_input
+        self.previous_input = user_input
+        return response
+
+
+scripts = {"doctor": doctor, "echo": echo}
 
 
 def load_script(script_name) -> elpyza_script:
-    return scripts[script_name]
+    selected = scripts[script_name]
+    return selected()
 
 
 def list_scripts() -> list:
